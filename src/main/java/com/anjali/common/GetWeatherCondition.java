d0317f16a0c4d801e68ff824613e436e1caf897e
anjali-g <40506193+anjali-g@users.noreply.github.com>
2018-07-27 14:41:08 -0700

    Update GetWeatherCondition.java

--- src/main/java/com/anjali/common/GetWeatherCondition.java
+++ src/main/java/com/anjali/common/GetWeatherCondition.java
@@ -1,3 +1,7 @@
+/*
+* An interface implemented by GetWeatheConditionImplementation where get request is made to Dark Sky API
+*/
+
 package com.anjali.common;
 
 public interface GetWeatherCondition {
